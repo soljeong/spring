cdda0f66b88889e265057a01863930b38a45886f
jeongsol <jeongsol@outlook.com>
2023-11-30 10:33:53 +0900

    테스트추가

--- src/test/java/com/example/demo3/Demo3ApplicationTests.java
+++ src/test/java/com/example/demo3/Demo3ApplicationTests.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,5 +29,14 @@ class Demo3ApplicationTests {
         Question q = all.get(0);
         assertEquals("sbb가 무엇인가요?", q.getSubject());
 	}
+
+	@Test
+    void testJpa2() {
+        Optional<Question> oq = this.questionRepository.findById(1);
+        if(oq.isPresent()) {
+            Question q = oq.get();
+            assertEquals("sbb가 무엇인가요?", q.getSubject());
+        }
+    }
 
 }
