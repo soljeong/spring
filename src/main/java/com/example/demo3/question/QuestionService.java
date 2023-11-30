f25ced9046bfd019fcd0280372d034e145fd7991
jeongsol <jeongsol@outlook.com>
2023-11-30 12:18:52 +0900

    상세페이지완성

--- src/main/java/com/example/demo3/question/QuestionService.java
+++ src/main/java/com/example/demo3/question/QuestionService.java
@@ -1,6 +1,9 @@
 package com.example.demo3.question;
 
+import com.example.demo3.DataNotFoundException;
+
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -16,6 +19,15 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {  
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 
 }
