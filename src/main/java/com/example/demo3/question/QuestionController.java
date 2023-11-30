e589c26969ebd55ccd31ae14999bde0b818c9eb0
jeongsol <jeongsol@outlook.com>
2023-11-30 11:40:24 +0900

    템플릿 조작

--- src/main/java/com/example/demo3/question/QuestionController.java
+++ src/main/java/com/example/demo3/question/QuestionController.java
@@ -1,16 +1,27 @@
 package com.example.demo3.question;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
-    @GetMapping("/question/list")
-    @ResponseBody
-    public String list() {
-        return "question list";
-    }
+    private final QuestionRepository questionRepository;
+    // @RequiredArgsConstructor는 롬복이 제공하는 애너테이션으로 final이 붙은 속성을 포함하는 생성자를 자동으로 생성하는 역할을 한다.
 
+
+    @GetMapping("/question/list")
+    public String list(Model model) {
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        // Model 객체는 자바 클래스와 템플릿 간의 연결고리 역할을 한다. Model 객체에 값을 담아두면 템플릿에서 그 값을 사용할 수 있다.
+        // Model 객체는 따로 생성할 필요없이 컨트롤러 메서드의 매개변수로 지정하기만 하면 스프링부트가 자동으로 Model 객체를 생성한다.
+        return "question_list";
+    }
 }
