e9ce50e819193f4a166a09b1f0f880fec83dd54f
jeongsol <jeongsol@outlook.com>
2023-11-30 10:30:38 +0900

    테스트코드변경

--- src/test/java/com/example/demo3/Demo3ApplicationTests.java
+++ src/test/java/com/example/demo3/Demo3ApplicationTests.java
@@ -1,7 +1,9 @@
 package com.example.demo3;
 
-import java.time.LocalDateTime;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDateTime;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,17 +21,12 @@ class Demo3ApplicationTests {
 
 	@Test
 	void testJpa() {
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1); // 첫번째 질문 저장
+        List<Question> all = this.questionRepository.findAll();
+        assertEquals(2, all.size());
+		// 데이터 사이즈가 2인지 확인하기 위해
 
-		Question q2 = new Question();
-		q2.setSubject("스프링부트 모델 질문입니다.");
-		q2.setContent("id는 자동으로 생성되나요?");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2); // 두번째 질문 저장
+        Question q = all.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
 	}
 
 }
