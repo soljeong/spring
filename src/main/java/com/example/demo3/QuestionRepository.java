85f19b19b9f0d341fd3d98c6cf0149effcd32782
jeongsol <jeongsol@outlook.com>
2023-11-30 10:39:51 +0900

    어떤 쿼리가 실행됐는지 볼 수 있도록

--- src/main/java/com/example/demo3/QuestionRepository.java
+++ src/main/java/com/example/demo3/QuestionRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
+    // 리포지터리 객체의 메서드가 실행될때 JPA가 해당 메서드명을 분석하여 쿼리를 만들고 실행한다.
 
 
 }
