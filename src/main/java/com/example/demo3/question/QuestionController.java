0ab8f427521aef6e5c520eafff7bfa635439edb8
jeongsol <jeongsol@outlook.com>
2023-11-30 12:09:43 +0900

    상세페이지 매핑

--- src/main/java/com/example/demo3/question/QuestionController.java
+++ src/main/java/com/example/demo3/question/QuestionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,5 +24,10 @@ public class QuestionController {
         // Model 객체는 자바 클래스와 템플릿 간의 연결고리 역할을 한다. Model 객체에 값을 담아두면 템플릿에서 그 값을 사용할 수 있다.
         // Model 객체는 따로 생성할 필요없이 컨트롤러 메서드의 매개변수로 지정하기만 하면 스프링부트가 자동으로 Model 객체를 생성한다.
         return "question_list";
+    }
+
+    @GetMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        return "question_detail";
     }
 }
