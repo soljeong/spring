f1ddd25363fb057363857fbd821d1e50e8992f1b
jeongsol <jeongsol@outlook.com>
2023-11-30 16:39:23 +0900

    중복아이디검사

--- src/main/java/com/example/demo3/user/UserController.java
+++ src/main/java/com/example/demo3/user/UserController.java
@@ -1,5 +1,6 @@
 package com.example.demo3.user;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,7 +30,20 @@ public class UserController {
         if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
             bindingResult.rejectValue("password2", "passwordInCorrect",
                     "2개의 패스워드가 일치하지 않습니다.");
-                    // bindingResult.rejectValue(필드명, 오류코드, 에러메시지)
+            // bindingResult.rejectValue(필드명, 오류코드, 에러메시지)
+            return "signup_form";
+        }
+
+        try {
+            userService.create(userCreateForm.getUsername(),
+                    userCreateForm.getEmail(), userCreateForm.getPassword1());
+        } catch (DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "signup_form";
+        } catch (Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
             return "signup_form";
         }
 
