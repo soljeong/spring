d29d6ad45335f63b5df259264c7331656e77d3a6
jeongsol <jeongsol@outlook.com>
2023-11-30 16:27:28 +0900

    새 엔터티

---
+++ src/main/java/com/example/demo3/user/SiteUser.java
@@ -0,0 +1,29 @@
+package com.example.demo3.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class SiteUser {
+
+    // User 대신 SiteUser로 한 이유는 스프링 시큐리티에 이미 User 클래스가 있기 때문
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+    
+}
