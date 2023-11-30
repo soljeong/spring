66aa2524b76afe005d40927d286518f717c7b8eb
jeongsol <jeongsol@outlook.com>
2023-11-30 13:44:31 +0900

    질문생성

--- src/main/java/com/example/demo3/question/QuestionController.java
+++ src/main/java/com/example/demo3/question/QuestionController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 
@@ -30,9 +32,17 @@ public class QuestionController {
 
     @GetMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
-        
+
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@RequestParam String subject, @RequestParam String content) {
+
+        this.questionService.create(subject, content);
+
+        return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
     }
 }
